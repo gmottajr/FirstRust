8016eec69d18cdc0558d3825417e76dbc6b87eaa
gerson <mottajrgerson@gmail.com>
2023-10-14 16:34:25 -0300

    learning print string and string interpolation

---
+++ gm-hash-map/src/main.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+    println!("**********************************************************");
+    println!("");
+    println!("{}", 4);
+    println!("Hey this is my {} experience with {}!!! It has not been that cool though!!", "first", "Rust");
+    println!("This is all that Hellen could teach me after almost {aHour} {caption} of tutorial!", aHour = 3, caption ="hours");
+    println!("{:?}", ("Gerson", 53, true));
+    println!("");
+    println!("**********************************************************");
+}
