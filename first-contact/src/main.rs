1d0317fd6387c1499f8b846320afd487a1a37dc1
gerson <mottajrgerson@gmail.com>
2023-10-14 16:48:10 -0300

    renaming project folder

---
+++ first-contact/src/main.rs
@@ -0,0 +1,27 @@
+
+fn main() {
+    println!("**********************************************************");
+    println!("");
+    println!("{}", 4);
+    println!("Hey this is my {} experience with {}!!! It has not been that cool though!!", "first", "Rust");
+    println!("This is all that Hellen could teach me after almost {aHour} {caption} of tutorial!", aHour = 3, caption ="hours");
+    println!("{:?}", ("Gerson", 22, true));
+    println!("");
+    println!("**********************************************************");
+
+    // Basic Mathematics
+    println!("");
+    println!("Basic Mathematics");
+    println!("");
+    println!("2 + 2 = {}", 2 + 2);
+    println!("2 - 2 = {}", 2 - 2);
+    println!("2 * 2 = {}", 2 * 2);
+    println!("2 / 2 = {}", 2 / 2);
+    println!("5 / 2 = {}", 5 / 2);
+    println!("2 % 2 = {}", 2 % 2);
+    println!("");
+    println!("Binary of 64: {:b}", 64);
+    println!("Binary of 10: {:b}", 10);
+    println!("Binary of 102: {:b}", 102);
+    println!("**********************************************************");
+}
